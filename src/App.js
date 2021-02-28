6166b21cda0ffd5c6e9b20cd87af84528b5009b0
Dalton Morgan <dnmorgan96@gmail.com>
2021-02-28 13:25:03 -0500

    added move functions, score keeping, and returning to first point

--- src/App.js
+++ src/App.js
@@ -2,58 +2,85 @@ import "./styles/App.css";
 import { useState } from "react";
 import Map from "./components/Map";
 import leafletPip from "leaflet-pip";
+import { L, GeoJson } from "leaflet";
+
+//------------------------------Lat Lon variables-----------------///
+let randLat = randLatGen(45.007561302382754, 42.730315121762715);
+let randLon = randLonGen(-73.26, -71.28);
+let playerScore = 100;
+let newCenter = [randLat, randLon]
+
+//---------------------------leaflet pip...idk-----------------------------//
+// let point = leafletPip.pointInLayer([randLon, randLat], );
+
+function randLonGen(min, max) {
+  let randLon = Math.random() * (max - min) + min;
+  return randLon;
+}
+
+function randLatGen(min, max) {
+  let randLat = Math.random() * (max - min) + min;
+  return randLat;
+}
+
+//--------------------deducts players point by the passed argument-----------//
+function playerDeductPoint(points) {
+  let newScore = playerScore - points;
+  playerScore = newScore;
+  console.log(newScore);
+}
+
+//----------------------------Main App Function---------------------------------//
 
 function App() {
-
   const [center, setCenter] = useState([43.88, -72.7317]);
-
   const [zoom, setZoom] = useState(8);
 
+  // console.log(persistentLat, persistentLon)
+
+  function startGame() {
+    toggleDisable();
+    setZoom(18);
+    // setCenter([randLat, randLon]);
+    setCenter(newCenter)
+    console.log(newCenter)
+  }
+
+  function toggleDisable() {
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("guess").disabled = false;
+    document.getElementById("quit").disabled = false;
+  }
+
   function MainButtons() {
+    //-------------------------move functions----------------//
 
-    //------------------------------Might Bring these variables now-----------------///
-    // let randLat = randLatGen(45.007561302382754, 42.730315121762715);
-    // let randLon = randLonGen(-73.26, -71.28);
-
-
-    //---------------------------leaflet pip...idk-----------------------------//
-    // let point = leafletPip.pointInLayer([-88, 38], gjLayer);
-
-    function randLonGen(min, max) {
-      let randLon = Math.random() * (max - min) + min;
-      return randLon;
+    function moveEast() {
+      randLon = randLon + 0.002;
+      setCenter([randLat, randLon]);
+      playerDeductPoint(1);
     }
 
-    function randLatGen(min, max) {
-      let randLat = Math.random() * (max - min) + min;
-      return randLat;
+    function moveWest() {
+      randLon = randLon - 0.002;
+      setCenter([randLat, randLon]);
+      playerDeductPoint(1);
     }
 
-    function toggleDisable() {
-      document.getElementById("start-button").disabled = true;
-      document.getElementById("guess").disabled = false;
-      document.getElementById("quit").disabled = false;
+    function moveNorth() {
+      randLat = randLat + 0.002;
+      setCenter([randLat, randLon]);
+      playerDeductPoint(1);
     }
 
+    function moveSouth() {
+      randLat = randLat - 0.002;
+      setCenter([randLat, randLon]);
+      playerDeductPoint(1);
+    }
 
-    //-------------------------move functions not working----------------//
-    // function north () {
-
-    // }
-
-    // function moveEast() {
-    //   let RandLat = randLat
-    //   let RandLon = randLon
-
-
-    //   setCenter([RandLat + .002, RandLon]);
-    // }
-
-    function startGame() {
-      toggleDisable();
-      setZoom(18);
-      // setCenter([randLat, randLon]); //commenting out to try to get buttons working.
-      setCenter([randLatGen(45.007561302382754, 42.730315121762715), randLonGen(-73.26, -71.28)])
+    function returnCenter() {
+      setCenter(newCenter)
     }
 
     function guess() {}
@@ -74,9 +101,13 @@ function App() {
           </button>
         </div>
 
-        {/* <div>
-          <button onClick={moveEast}>east</button>
-        </div> */}
+        <div id="nav-buttons">
+          <button onClick={moveEast}>East</button>
+          <button onClick={moveWest}>West</button>
+          <button onClick={moveNorth}>North</button>
+          <button onClick={moveSouth}>South</button>
+          <button onClick={returnCenter}>Return to First Point</button>
+        </div>
       </>
     );
   }
---
+++ src/components/ScoreMath.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+
+
+// export default function FindScore(props) {
+
+//     let playerDeductPoint = (points) => {
+//         let newScore = playerScore - points;
+//         playerScore = newScore
+//         console.log(newScore)
+//       }
+
+
+    
+// }
+
+
+//-----------------possible react stuff later-----------------//
